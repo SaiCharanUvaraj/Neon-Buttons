cf5fe9b47b12db3d26b5bc64e9d200ed14a836b7
Sai Charan Uvaraj <saicharanuvi01@gmail.com>
2024-03-12 23:18:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function  lableAnimation()
+{
+    var lable = document.getElementById("lable");
+    lable.style.fontSize="3rem";
+    lable.style.transition="2s";
+    document.getElementById("Black").classList.add("Black");
+    document.getElementById("Orange").classList.add("Orange");
+    document.getElementById("Magenta").classList.add("Magenta");
+    document.getElementById("Purple").classList.add("Purple");
+}
